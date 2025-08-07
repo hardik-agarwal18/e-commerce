419231c21c39a6975d7fdb0e5ba169f5f7bb6228
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-08-07 12:03:37 +0530

    fix : Change userId type in Cart model from String to ObjectId for proper reference

--- backend/models/CartModel.js
+++ backend/models/CartModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const cartSchema = new mongoose.Schema(
   {
     userId: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
